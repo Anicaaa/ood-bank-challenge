9449b3cc8cb3938f94097c695c782c636cf76791
Anica Rahman <anicarahman2009@yahoo.it>
2022-04-07 15:53:41 +0100

    Passed total balance and updated withdrawal

--- spec/bank.spec.js
+++ spec/bank.spec.js
@@ -32,8 +32,14 @@ describe("Bank class", () => {
   it("should accept my first withdrawal in bank account and save it in the transaction list", () => {
     //set up
     const account = new Bank();
+    account.deposit(1000, "06-04-2022");
     //execute
     const objectList = [
+      {
+        amount: 1000,
+        date: "06-04-2022",
+        transactionType: "deposit",
+      },
       {
         amount: 50,
         date: "10-04-2022",
@@ -52,6 +58,32 @@ describe("Bank class", () => {
     //execute
     const objectList = [];
     const result = account.withdrawal(0, "10-04-2022");
+    //verify
+    expect(result).toEqual(false);
+    expect(account.transactions).toEqual(objectList);
+  });
+
+  it("show total balance after depositing and withdrawing money from bank account", () => {
+    //set up
+    const account = new Bank();
+    //execute
+    account.transactions = [
+      { amount: 200, date: "13-07-2022", transactionType: "deposit" },
+      { amount: 150, date: "14-07-2022", transactionType: "deposit" },
+      { amount: 100, date: "15-07-2022", transactionType: "withdrawal" },
+    ];
+    const expected = 250;
+    const result = account.totalBalance();
+    //verify
+    expect(result).toEqual(expected);
+  });
+
+  it("should reject a withdrawal if I have no money", () => {
+    //set up
+    const account = new Bank();
+    //execute
+    const objectList = []; // cannot see a withdrawal transaction because I have no money in bank account
+    const result = account.withdrawal(50, "10-04-2022");
     //verify
     expect(result).toEqual(false);
     expect(account.transactions).toEqual(objectList);
--- src/bank.js
+++ src/bank.js
@@ -16,8 +16,15 @@ class Bank {
     this.transactions.push(firstTransaction);
     return true;
   }
+
   withdrawal(amountWithdrawn, dateWithdrawn) {
     if (amountWithdrawn <= 0) {
+      return false;
+    }
+    // store total balance in a variable
+    const totalMoney = this.totalBalance();
+    // Check if the money I want to take is more than what I have inside my account.
+    if (amountWithdrawn > totalMoney) {
       return false;
     }
 
@@ -29,6 +36,24 @@ class Bank {
     this.transactions.push(secondTransaction);
     return true;
   }
+
+  totalBalance() {
+    let total = 0;
+    for (let i = 0; i < this.transactions.length; i++) {
+      // Get the transaction from the list at index[i]
+      // Towards that transaction in a variable called transaction
+      // { amount: 200, date: "13-07-2022", transactionType: "deposit" } this is an object containing 3 properties.
+      const transactionObject = this.transactions[i];
+      if (transactionObject.transactionType === "deposit") {
+        total += transactionObject.amount;
+      }
+      if (transactionObject.transactionType === "withdrawal") {
+        total -= transactionObject.amount;
+      }
+    }
+    return total;
+  }
+
   bankStatement() {}
 }
 
